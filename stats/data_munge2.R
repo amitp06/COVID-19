0d8b642d6c204bafe1a85d4702181871f2e736ef
amitp06 <70465064+amitp06@users.noreply.github.com>
2020-10-02 11:57:39 -0700

    Check model performance and select logistic

--- stats/data_munge2.R
+++ stats/data_munge2.R
@@ -2,6 +2,7 @@ require(dplyr)
 require(readr)
 require(caret)
 require(pROC)
+require(PRROC)
 require(lubridate)
 require(leaps)
 
@@ -59,8 +60,10 @@ df_agg = df_subset %>%
 df_agg$case_diff_7_8 = df_agg$end_cases_8/df_agg$end_cases_7 - 1
 df_agg$case_diff_7_8[is.na(df_agg$case_diff_7_8)] = df_agg$end_cases_8[is.na(df_agg$case_diff_7_8)] - 1
 
-cutoff = 0.3409769 # Hardcoded median but could be an arbitrary value
-df_agg$high_growth_8 = as.factor(df_agg$case_diff_7_8 > cutoff)
+# Hardcoded county median as response variable cutoff but could be an arbitrary value
+cutoff = 0.3409769
+df_agg$high_growth_8 = factor(ifelse(df_agg$case_diff_7_8 > cutoff,'high_growth','low_growth'))
+df_agg$high_growth_8 = relevel(df_agg$high_growth_8,ref='low_growth')
 
 df_agg$retail_diff_7_8 = (df_agg$median_retail_8 - df_agg$median_retail_7)
 df_agg$grocery_diff_7_8 = (df_agg$median_grocery_8 - df_agg$median_grocery_7)
@@ -68,12 +71,12 @@ df_agg$parks_diff_7_8 = (df_agg$median_parks_8 - df_agg$median_parks_7)
 df_agg$transit_diff_7_8 = (df_agg$median_transit_8 - df_agg$median_transit_7)
 df_agg$work_diff_7_8 = (df_agg$median_work_8 - df_agg$median_work_7)
 df_agg$res_diff_7_8 = (df_agg$median_res_8 - df_agg$median_res_7)
-df_agg$ind_retail = as.factor(df_agg$median_retail_8 > median(df_agg$median_retail_8))
-df_agg$ind_grocery = as.factor(df_agg$median_grocery_8 > median(df_agg$median_grocery_8))
-df_agg$ind_parks = as.factor(df_agg$median_parks_8 > median(df_agg$median_parks_8))
-df_agg$ind_transit = as.factor(df_agg$median_transit_8 > median(df_agg$median_transit_8))
-df_agg$ind_work = as.factor(df_agg$median_work_8 > median(df_agg$median_work_8))
-df_agg$ind_res = as.factor(df_agg$median_res_8 > median(df_agg$median_res_8))
+# df_agg$ind_retail = as.factor(df_agg$median_retail_8 > median(df_agg$median_retail_8))
+# df_agg$ind_grocery = as.factor(df_agg$median_grocery_8 > median(df_agg$median_grocery_8))
+# df_agg$ind_parks = as.factor(df_agg$median_parks_8 > median(df_agg$median_parks_8))
+# df_agg$ind_transit = as.factor(df_agg$median_transit_8 > median(df_agg$median_transit_8))
+# df_agg$ind_work = as.factor(df_agg$median_work_8 > median(df_agg$median_work_8))
+# df_agg$ind_res = as.factor(df_agg$median_res_8 > median(df_agg$median_res_8))
 
 anyNA(df_agg)
 
@@ -113,15 +116,31 @@ summary(stepwise_lm)
 # Train experimental classifiers and check performance
 
 set.seed(1)
-experimental = train(high_growth_8 ~ mean_work_8 + mean_parks_8, 
+train_experimental = trainControl(method="cv",number=10,classProbs=TRUE,savePredictions=TRUE)
+experimental = train(high_growth_8 ~ population + retail_diff_7_8 + grocery_diff_7_8 + parks_diff_7_8
+                     + transit_diff_7_8 + work_diff_7_8 + res_diff_7_8 + mean_retail_8 + mean_grocery_8
+                     + mean_parks_8 + mean_transit_8 + mean_work_8 + mean_res_8, 
                     data = df_agg,
-                    method = 'glm', 
-                    #family = 'binomial',
-                    #tuneGrid = data.frame(nvmax = 1:5),
-                    trControl = train.control
+                    method = 'glmStepAIC',
+                    trControl = train_experimental
 )
 summary(experimental)
+
+# Get confusion matrix stats
 confusionMatrix(experimental)
 precision = confusionMatrix(experimental)$table[2,2]/sum(confusionMatrix(experimental)$table[2,])
 recall = confusionMatrix(experimental)$table[2,2]/sum(confusionMatrix(experimental)$table[,2])
 precision;recall
+
+# Get ROC stats
+
+roc_curve = roc(experimental$pred$obs,experimental$pred$high_growth)
+plot(roc_curve)
+roc_curve$auc
+
+# Get calibration stats
+calibration = calibration(experimental$pred$obs~experimental$pred$high_growth,cuts=5,class='high_growth')
+xyplot(calibration)
+
+# Save model candidate (only use when you want to keep a model for faster loading in a future script)
+# saveRDS(experimental,'logistic_selected.rds')
