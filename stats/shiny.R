10c9470ae0ebf96ff46e0f97542e33f342a8566f
Andrew Somers <aksomers@gmail.com>
2020-11-27 19:08:31 -0500

    create shiny2 to be closer to vision

--- stats/shiny.R
+++ stats/shiny.R
@@ -17,8 +17,7 @@ ui <- fluidPage(
                   selected=choices[1])
     ),
     mainPanel(
-      plotOutput("County"),
-      plotOutput("Total")
+      plotOutput("County")
     )
   )
 )
@@ -28,10 +27,7 @@ server <- function(input, output) {
     plot(df_agg2[df_agg2$location_combined==input$County,]$mobility_workplaces_change,df_agg2[df_agg2$location_combined==input$County,]$cases_inc,
          xlab="Mobility Change for Workplaces in the County", ylab = "Incremental Case Counts Change in the County")
   })
-  output$Total <- renderPlot({
-    plot(df_agg$mean_work_7,df_agg$mean_cases_8,
-         xlab="Mean Mobility Change for Workplaces in July for the US", ylab = "Mean Case Count Change in August for the US")
-  })
+
 }
 
 shinyApp(ui = ui, server = server)
---
+++ stats/shiny2.R
@@ -0,0 +1,38 @@
+library(shiny)
+#ui <- fluidPage()
+
+#server <- function(input, output) {}
+
+#shinyApp(ui = ui, server = server)
+
+choices = c('Retail and Recreation Mobility', 'Grocery and Pharmacy Mobility', 'Parks Mobility',
+            'Transit Stations Mobility', 'Workplace Mobility', 'Residential Mobility')
+
+df_agg2 = df_agg[,c('mean_retail_7', 'mean_grocery_7', 'mean_parks_7', 'mean_transit_7','mean_work_7', 'mean_res_7','case_diff_7_8')]
+
+choicelookup = as.data.frame(cbind(choices, c('mean_retail_7', 'mean_grocery_7', 'mean_parks_7', 'mean_transit_7','mean_work_7', 'mean_res_7')))
+
+ui <- fluidPage(
+  
+  sidebarLayout(
+    sidebarPanel(
+      selectInput(inputId='Category',
+                  label = 'Mobilty Category',
+                  choices = choices,
+                  selected=choices[1])
+    ),
+    mainPanel(
+      plotOutput("Category")
+    )
+  )
+)
+
+server <- function(input, output) {
+  output$Category <- renderPlot({
+    plot(data.frame(df_agg2[,choicelookup[choicelookup$choices==input$Category,]$V2],df_agg2[,"case_diff_7_8"]),
+         xlab="Mean July Mobility Change for Selected Category", ylab = "Percentage Case Count Change from July to August")
+  })
+
+}
+
+shinyApp(ui = ui, server = server)
